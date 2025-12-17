40720ed6705ac8a7775fdca2eb4433317d29e9ce
bala-amypo <bala@amypo.in>
2025-12-17 09:40:16 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,8 +1,6 @@
 package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
+import jakarta.persistence.*;
 
 @Entity 
 public class Student{
@@ -22,17 +20,18 @@ public class Student{
 
     public String getName(){  
         return name;
-    
     }
     public void setName(String name){
         this.name=name;
     }
-    // public String getEmail(){
-    //     return email;
-    // }
-    // public void setEmail(String email){
-    //     this.email=email;
-    // }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    
     public float cgpa(){
         return cgpa;
     }
